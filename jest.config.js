c19370fcfda87898c6e4b6a5c7c376005fddbd8f
danielrubens <danielrubensdaniel@gmail.com>
2022-11-27 01:56:53 -0300

    chore: project configured

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    // collectCoverage: true,
+    roots: ['<rootDir>/src'],
+    collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+    testEnvironment: 'jest-environment-node',
+    transform: {
+      '.+\\.ts$': 'ts-jest'
+    },
+    roots: ['<rootDir>/src']
+    // jest --clearCache
+  }
